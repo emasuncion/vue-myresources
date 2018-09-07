a6cab4c868350dfe9afb050d339ec5a122c5950b
Eleirold Asuncion <easuncion@cambridge.org>
2018-09-07 09:56:39 +0800

    fix error in declaration of router in main.js
    Signed-off-by: Eleirold Asuncion <easuncion@cambridge.org>

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import Vuex from 'vuex'
 import router from './router'
 import store from './store'
-import router from './router'
 require('@/assets/css/go_core.css')
 require('@/assets/css/style.css')
 require('@/assets/css/bootstrap.min.css')
